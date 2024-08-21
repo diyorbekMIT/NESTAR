a6666b2744c2e8a915dae8bb29d03e6b24b0aeee
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-08-21 20:36:32 +0900

    feat: sign up and login development

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,25 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Model } from 'mongoose';
+import { MemberStatus } from '../../libs/enums/member.enum';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class MemberService {
-        constructor(
-          @InjectModel('Member') private readonly memberModel: Model<null>,
-        ) {}
-        public async signup(): Promise<string> {
-          return 'signup executed!';
+
+    constructor(@InjectModel("Member") private readonly memberModel: Model<Member>) {}
+
+    public async signup(input: MemberInput): Promise<Member> {
+        //TODO: Hash password
+        try {
+            const result = await this.memberModel.create(input);
+            //Todo: Authentication via Token
+            return result;
+            
+        } catch(err) {
+            console.log("Error, Service.model:", err);
+            throw new BadRequestException(err)  
         }
-      
-        public async login(): Promise<string> {
-          return 'login executed!';
-        }
-      
-        public async updateMember(): Promise<string> {
-          return 'updateMember executed!';
-        }
-      
-        public async getMember(): Promise<string> {
-          return 'getMember executed!';
-        }
+        
+    }
+
+    public async login(input: LoginInput): Promise<Member> {
+        const { memberNick, memberPassword } = input;
+        const response: Member = await this.memberModel
+            .findOne({memberNick})
+            .select('+memberPassword')
+            .exec()
+
+            if(!response || response.memberStatus === MemberStatus.DELETE) {
+                throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+            }else if(response.memberStatus === MemberStatus.BLOCK) {
+                throw new InternalServerErrorException(Message.BLOCKED_USER)
+            }
+
+            //TOdo: Compare password
+            console.log("responce:", response )
+            const isMatch = memberPassword === response.memberPassword;
+            if(!isMatch) 
+                throw new InternalServerErrorException(Message.WRONG_PASSWORD)
+
+        return response;
+    }
+
+    public async updateMember(): Promise<string> {
+        return 'updateMember executed'
+    }
+
+    public async getMember(): Promise<string> {
+        return 'getMember executed'
+    }
+
 }
--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -1,14 +1,19 @@
 export enum Message {
-    SOMETHING_WENT_WRONG = "Something went wrong",
-    NO_DATA_FOUND = "No data found",
-    CREATE_FAILED = "Create is failed",
-    UPDATE_FAILED = "Update is failed",
-
-    BLOCKED_USER = "You are blocked, contact admin or reastarurant",
-    USED_NICK_PHONE = "You are inserting already used nick or phone",
-    NO_ERROR_NICK = "No member with that member nick",
-    WRONG_PASSWORD = "Wrong Password inserted, please try again",
-    NOT_AUTHONTICATED = "NOT AUTHONTICATED",
-    TOKEN_CREATION_FAILED = "Token creation failed",
-    INVALID_TOKEN = "INVALID_TOKEN"
-}
+    SOMETHING_WENT_WRONG = 'Something went wrong!',
+    NO_DATA_FOUND = 'No data found!',
+    CREATE_FAILED = 'Create failed!',
+    UPDATE_FAILED = 'Update failed!',
+    REMOVE_FAILED = 'Remove failed!',
+    UPLOAD_FAILED = 'Upload failed!',
+    BAD_REQUEST = 'Bad Request',
+    
+    NO_MEMBER_NICK = 'No member with that member nick!',
+    BLOCKED_USER = 'You have been blocked!',
+    WRONG_PASSWORD = 'Wrong password, try again!',
+    NOT_AUTHENTICATED = 'You are not authenticated, please login first!',
+    TOKEN_NOT_EXIST = 'Bearer Token is not provided!',
+    ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles!',
+    NOT_ALLOWED_REQUEST = 'Not Allowed Request!',
+    PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, ipeg or png images!',
+    SELF_SUBSCRIPTION_DENIED = 'Self subscription is denied!',
+    }
