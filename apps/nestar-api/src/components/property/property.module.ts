fcb7fd65afafff6472cb40b017eee0a131688c6a
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-09-01 17:14:47 +0900

    feat: corrected version

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PropertyResolver } from './property.resolver';
 import { PropertyService } from './property.service';
-import MemberSchema from '../../schemas/Member.model';
-import { ViewModule } from '../view/view.module';
-import { AuthModule } from '../auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import PropertySchema from '../../schemas/Property.model';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
 import { MemberModule } from '../member/member.module';
 
 @Module({
-  imports:  [MongooseModule.forFeature([{ name: 'Property', schema: MemberSchema }]), AuthModule, ViewModule, MemberModule],
-  providers: [PropertyResolver, PropertyService]
+	imports: [MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]), AuthModule, ViewModule, MemberModule],
+	providers: [PropertyResolver, PropertyService],
 })
 export class PropertyModule {}
