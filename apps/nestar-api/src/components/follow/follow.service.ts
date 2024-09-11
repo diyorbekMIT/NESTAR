1ce0f839b9a63ce6c0f3d1e87d1f416ec3c3b70b
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-09-11 20:47:48 +0900

    feat: modify like interaction related logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -8,7 +8,7 @@ import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { internalExecuteOperation } from '@apollo/server/dist/esm/ApolloServer';
 import { T } from '../../libs/types/common';
-import { lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 
 @Injectable()
 export class FollowService {
@@ -75,6 +75,7 @@ export class FollowService {
                             [
                                 { $skip: (page - 1) * limit },
                                 { $limit: limit },
+                                lookupAuthMemberLiked(memberId, "$followingId"),
                                 lookupFollowingData,
                                 { $unwind: '$followingData' },
                             ],
@@ -105,6 +106,7 @@ export class FollowService {
                             [
                                 { $skip: (page - 1) * limit },
                                 { $limit: limit },
+                                lookupAuthMemberLiked(memberId),
                                 lookupFollowerData,
                                 { $unwind: '$followerData' },
                             ],
